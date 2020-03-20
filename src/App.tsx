46c42c289302a300a2241a1165e13611211d2da0
Thiago Cruz <thiagocruzsantos@gmail.com>
2020-03-20 17:01:35 -0300

    theme and base component for game

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled, { ThemeProvider } from "styled-components";
+import upcar from "./carro.png";
+import cenario from "./cenario.gif";
+import { Theme } from "./theme";
+
+export interface IUpcar {
+  lane: "left" | "middle" | "right";
+}
+
+const Background = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: url(${cenario}) center no-repeat;
+  background-size: cover;
+`;
+
+const Upcar = styled.div<IUpcar>`
+  width: 25vw;
+  height: 25vh;
+  background: url(${upcar}) 0 0 no-repeat;
+  background-size: contain;
+  position: absolute;
+  bottom: 1rem;
+  left: ${props =>
+    (props.theme.lanes as { [key: string]: string })[props.lane]};
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={Theme}>
+      <Background>
+        <Upcar lane="middle"></Upcar>
+      </Background>
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme.ts
@@ -0,0 +1,11 @@
+import { DefaultTheme } from "styled-components";
+
+const Theme: DefaultTheme = {
+  lanes: {
+    left: "10%",
+    middle: "41.5%",
+    right: "65%"
+  }
+};
+
+export { Theme };
