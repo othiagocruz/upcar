b3bbbc64642dee4bc819f3700633e9b710e32026
Thiago Cruz <thiagocruzsantos@gmail.com>
2020-03-22 22:44:28 -0300

    Updates

---
+++ precache-manifest.9ef9cdf7acc05998116f6dea4024a480.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43914cf868c39ffaeda9f32e162a5980",
+    "url": "/upcar/index.html"
+  },
+  {
+    "revision": "c0d482e02dac2a234622",
+    "url": "/upcar/static/css/main.3e45ec72.chunk.css"
+  },
+  {
+    "revision": "aec28e89125153adf19a",
+    "url": "/upcar/static/js/2.75905c61.chunk.js"
+  },
+  {
+    "revision": "702e06c21f4db63e7e05a8a0eda9baf1",
+    "url": "/upcar/static/js/2.75905c61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0d482e02dac2a234622",
+    "url": "/upcar/static/js/main.e2a59227.chunk.js"
+  },
+  {
+    "revision": "6926af888a2c64d44dcb",
+    "url": "/upcar/static/js/runtime-main.f14bcba4.js"
+  },
+  {
+    "revision": "56be0a4e7be4b661072436aa22f66f64",
+    "url": "/upcar/static/media/carro.56be0a4e.png"
+  },
+  {
+    "revision": "d0d2cd1752d215c4df68e76e976ec629",
+    "url": "/upcar/static/media/cenario.d0d2cd17.gif"
+  }
+]);
